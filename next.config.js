298f7004fc5618dd0c3979c88ea4b14b6ccb1574
“ptrenda” <treendaa@gmail.com>
2023-06-06 20:08:22 +0200

    add logging

--- next.config.js
+++ next.config.js
@@ -4,9 +4,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
 let basePath = '/'
-
+console.log('isGithubActions',isGithubActions,);
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+console.log('repo',repo,);
 
   assetPrefix = `/${repo}/`
   basePath = `/${repo}`
